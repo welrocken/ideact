df6c104faf91814a7bd28eef3a424836137a0b0e
Musa Ahmetcan Türker <ahmetcantrker@yahoo.com>
2017-12-09 13:50:33 +0300

    d

--- src/domain/Domain/IIdea.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ideact.Domain
-{
-    public interface IIdea
-    {
-        long Id { get; set; }
-        IProblem Problem { get; set; }
-        ISolution Solution { get; set; }
-    }
-}
--- src/domain/Domain/ISolution.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ideact.Domain
-{
-    public interface ISolution
-    {
-        long Id { get; set; }
-        string Title { get; set; }
-        string Description { get; set; }
-    }
-}
